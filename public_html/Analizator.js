eea8b33fc835157b168094bc3ad95805d6263baf
Guillermo Casas <g.casas.r94@gmail.com>
2020-05-21 18:12:16 +0200

    REGEX210520
    Funcionamiento del programa finalizado.
    Por revisar: 
    - Las expresiones regulares de las palabras buenas y malas (JSON)
    - Aspecto visual (CSS)

--- public_html/Analizator.js
+++ public_html/Analizator.js
@@ -1,35 +1,118 @@
-Analizator = function(){
-    this.REGEX;
-    this.modificadores;
-    this.analizarOld=function(mensaje,regular,modi){
-        console.log("Funcion analizar OLD. Mensaje: "+mensaje+" // regular: "+regular+" // modi: "+modi);
-        rg = new RegExp(regular,modi);
-        return mensaje.match(rg);
-    };
+Analizator = function(){   
+    // =====================================
+    // ========= MÉTODOS PROPIOS ===========
+    // =====================================
     
-    // Metodo no usado
-    this.analizar=function(mensaje,regular,modi){
-        console.log("Funcion analizar. Mensaje: "+mensaje+" // regular: "+regular+" // modi: "+modi);
+    /**
+     * Analizador básico. Devuelve el resultado de un análisis de expresión regular
+     * de un texto
+     * @param {String} mensaje Texto a analizar
+     * @param {String} regular Expresion regular que buscar en mensaje
+     * @param {String} modi Modificadores [i,g,l]
+     * @returns {Array} Lista de las expresiones encontradas
+     */
+    this.analiBasic=function(mensaje,regular,modi){
         var rg = new RegExp(regular,modi);
-        var resultado="";
+        return mensaje.match(rg);
+    }
+    
+    /**
+     * Analizador completo. Si se entrega una expresión regular a analizar su
+     * ejecución ser la del analizador básico. Si se entrega una lista de expresiones regulares,
+     * recorrera esa lista y entregará una lista con todos los resultados obtenidos
+     * @param {String} mensaje Texto a analizar
+     * @param {Array|String} regular Expresion/es regular que buscar en mensaje
+     * @param {String} modi Modificadores [i,g,l]
+     * @returns {Array} Lista de las expresiones encontradas
+     */
+    this.analizarComp=function(mensaje,regular,modi="gi"){
+        var resultado;
         
         if(typeof regular === "string"){
             console.log("Es una cadena de texto");
-            resultado = mensaje.match(rg);
+            resultado = this.analiBasic(mensaje,regular,modi);
 
         }else if(typeof regular === 'object'){
             console.log('Es un objeto');
-            
+            resultado=[];
             for(var i in regular){
-                rg = new RegExp(regular[i],modi);
-                var aux = mensaje.match(rg);
-                if (aux !== null){
-                    resultado += ' / '+ aux + '/';
+                aux = this.analiBasic(mensaje,regular[i],modi);
+                if(aux !== null){
+                    resultado = resultado.concat(aux);
                 }
             }
         }
+        console.log("Resultado: " + resultado);
         return resultado;
     };
+    
+    /**
+     * Método para obtener la suma de todos los números encontrados en un texto
+     * @param {String} mensaje Texto a analizar
+     * @param {String} regular Números que buscar en mensaje
+     * @param {String} modi Modificadores [i,g,l]
+     * @returns {Number} Suma de los números encontrados
+     */
+    this.analiSum = function(mensaje,regular="\\d+",modi="gi"){
+        console.log("Método de análisis de suma");
+        console.log("Regular: " + regular + " / Modi: " + modi);
+        var numS = this.analiBasic(mensaje,regular,modi);
+        var operacion = "0";
+        var resultado;
+        for(var i in numS){
+            operacion += '+' + numS[i];
+        }
+        console.log("Operación: " + operacion);
+        try{
+            resultado = eval(operacion);
+        }catch(Exeption){
+            resultado = 0;
+        }
+        return resultado;
+    };
+    
+    /**
+     * Método para analizar grupos de expresiones por separado. Devolverá un texto
+     * ya formateado:<br><br>
+     * Nombre de lista
+     * <ui>
+     *      <li>Palabras encontradas</li>
+     *      <li>Número de palabras encontradas</li>
+     *      <li>% encontradas / Total encontradas</li>
+     *      <li>% Encontradas / Total de palabras </li>
+     * </ui>
+     * @param {String} mensaje Mensaje a analizar
+     * @param {Array} regular Listas de RegEx
+     * @param {String} modi Modificadores
+     * @returns {String} Resultado ya formateado del análisis
+     */
+    this.analiTipolo = function(mensaje,regular,modi="gi"){
+        // Variables locales
+        var listas = Object.keys(regular);
+        var resultados = [];
+        var salida="";
+        for (var i in listas){
+            resultados[i] = this.analizarComp(mensaje,regular[listas[i]],modi);
+        }
+        
+        var palabTotal = mensaje.split(" ").length;
+        var palabEncont = resultados[0].length + resultados[1].length;
+        
+        for (var i in resultados){
+            var porcEncon = (100*resultados[i].length/palabEncont).toFixed(2);
+            var porcTotal = (100*resultados[i].length/palabTotal).toFixed(2);
+            if(isNaN(porcEncon)){porcEncon=0;}
+            if(isNaN(porcTotal)){porcTotal=0;}
+            
+            salida+="<u>Palabras: <i>" + listas[i] + "</i></u>" +
+                    "<ui>"+ 
+                    "<li><b>Palabras encontradas:</b> " + resultados[i] +
+                    "<li><b>Número de palabras encontradas:</b> "+ resultados[i].length + "</li>" +
+                    "<li><b>% Encontradas / Total encontradas: </b> "+ porcEncon + "%</li>" +
+                    "<li><b>% Encontradas / Total:</b> " + porcTotal + "%</li>" +
+                    "</ui><br>"
+            ;
+        }
+        return salida;
+    };
 };
-
-
--- public_html/motor.js
+++ public_html/motor.js
@@ -1,16 +1,30 @@
-// VARIABLES DE SESION
-let IDtext = document.getElementById('texto');
-let Sfech = document.getElementById('salFech');
-let Semail = document.getElementById('salEmail');
-let Sname = document.getElementById('salName');
-let Stipol = document.getElementById('salTipol');
-let Ssum = document.getElementById('salSum');
-var salidas = [Sfech,Semail,Sname,Stipol,Ssum];
-var analitc = new Analizator();
-var regexJ;
+// VARIABLES DE SESIÓN
+// Contenedor del texto
+let IDtext  = document.getElementById('texto'); 
 
+// Salidas de resultados
+let Sfech   = document.getElementById('salFech');
+let Semail  = document.getElementById('salEmail');
+let Sname   = document.getElementById('salName');
+let Stipol  = document.getElementById('salTipol');
+let Ssum    = document.getElementById('salSum');
+
+// Librería de contenedores de salidas
+var salidas = [Sfech,Semail,Sname,Stipol,Ssum];
+
+// Objeto analizador
+var analitc = new Analizator();
+
+// Variable donde almacenamos las RegEx del servidor
+var regexJ; 
+
+// Valor del modificador en todas las consultas
 let modi = 'gi';
 
+/**
+ * Función para obtener los RegEx del .json del servidor.
+ * Lo almacenaremos en la variable regexJ (ya definida)
+ */
 var lecturaJSON = function(){
     fetch('regexJ.json')
         .then(function(response){
@@ -23,23 +37,39 @@ var lecturaJSON = function(){
     );
 };
 
+/**
+ * Función principal. Se iniciará al pulsar un botón. Se enviará como parámetro 
+ * el código numérico del botón.
+ * En función del código de botón se decodifica al nombre de la lista a la que
+ * pertenecen en el regex.json (en la lista deCode) y en función de que lista a la que pertenezca
+ * se ejecutará la función:
+ * - Extracción de las palabras coincidentes
+ * - Análisis tipológico del texto
+ * - Suma de los números existente en el texto
+ * @param {Numeric} ty Código numérico del botón
+ */
 var aniButt = function(ty){
-    console.log('ejecucion funcion aniButt');
-    // decodificamos el ty al nombre clave del JSON 
-    var n = regexJ.deCode[ty];
-    console.log("Tipo de elementos: " + typeof regexJ[n]);
-    // Extraemos el resultado del Regular expresion
-    var Msalida = analitc.analizar(IDtext.value,regexJ[n],modi);
-     
-    if( ty == 4 ){
-        var numS = analitc.analizar(IDtext.value,regexJ[n],modi);
-        var operacion = '';
-        for(var i in numS){
-            operacion += '+' + numS[i];
-        }
-        Msalida = eval(operacion);
+    console.log('Ejecucion funcion principal (aniButt). Codigo: ' + ty);
+    // Decodificamos el ty al nombre clave del JSON
+    var deTy = regexJ.deCode[ty];
+    // Definimos e iniciamos la variable de salida
+    var Msalida="";
+    
+    // Elección de la ejecución en función de la lista seleccionada
+    if(deTy === "fecha" || deTy === "email" || deTy === "nombre"){  
+        Msalida = analitc.analizarComp(IDtext.value,regexJ[deTy],modi);
+    }else if(deTy === "tipologia"){
+        Msalida = analitc.analiTipolo(IDtext.value,regexJ[deTy]);
+    }else if(deTy === "suma"){
+        Msalida = analitc.analiSum(IDtext.value);
+    }
+    
+    // Si el resultado es nulo, corregimos el mensaje a un mensaje estandar
+    if(Msalida === null || Msalida.length<1){
+        Msalida="Sin resultados";
     }
     salidas[ty].innerHTML = Msalida;
 };
 
+// Evento para cargar la librería de RegEx del archivo .json
 document.addEventListener('onload',lecturaJSON());
